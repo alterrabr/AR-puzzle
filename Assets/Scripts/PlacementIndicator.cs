1d3182c60409aee11b369e9c8c1ca5930b2b1c03
Max <alterra.br@gmail.com>
2020-09-01 19:17:10 +0700

    Add placement indicator #ARPZ-2

---
+++ Assets/Scripts/PlacementIndicator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlacementIndicator : MonoBehaviour
+{
+    [SerializeField]
+    private ARRaycastManager rayManager;
+
+    [SerializeField]
+    private GameObject visual;
+
+    private void Start()
+    {
+        // Hide the placement visual
+        visual.SetActive(false);
+    }
+
+    private void Update()
+    {
+        // Shoot a raycast from center of the screen
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+        rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+        // if hit AR plane, update the position and rotation
+        if (hits.Count > 0)
+        {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+
+            if (!visual.activeInHierarchy)
+            {
+                visual.SetActive(true);
+            }
+        }
+    }
+}
